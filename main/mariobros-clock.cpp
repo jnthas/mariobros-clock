9907bf11f8e38c3de4d0f32e2331fd345bba1f98
Avi <avibrender@gmail.com>
2022-09-25 16:32:43 -0400

    Add menu configuration option to swap Blue and Green colors.
    The HUB75 64x64 display that I purchased from Aliexpress had the B & G colors swapped. This seems to be a common problem that people are facing.
    
    This code change adds code that will swap the B & G pins, as well as a menu configuration for esp-idf that makes it easy to enable/disable.

--- main/mariobros-clock.cpp
+++ main/mariobros-clock.cpp
@@ -3,4 +3,10 @@
    so get around this limitation we create a .cpp file which includes the .ino file.
 */
 
+// Include the project configuration.
+// See https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/kconfig.html for details.
+#include <sdkconfig.h>
+
+#define HUB75_BLUE_GREEN_SWAP CONFIG_HUB75_BLUE_GREEN_SWAP
+
 #include "../mariobros-clock.ino"
